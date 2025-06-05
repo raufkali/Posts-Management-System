966c0268fd17d9390283cae4a46865c3f0140236
raufkali <ahmadraufbd@gmail.com>
2025-06-05 21:09:42 +0500

    add navbar to layout

--- client/app/layout.jsx
+++ client/app/layout.jsx
@@ -1,5 +1,6 @@
 // app/layout.jsx
 import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "@/components/navbar";
 
 export const metadata = {
   title: "Post Management System",
@@ -9,15 +10,8 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="bg-light">
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-          <div className="container">
-            <a className="navbar-brand" href="/">
-              Post Manager
-            </a>
-          </div>
-        </nav>
-        <main className="container py-4">{children}</main>
+      <body>
+        <Navbar />
       </body>
     </html>
   );
